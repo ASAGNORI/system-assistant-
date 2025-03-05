51c1c90e6bd6f62151e73b4f5188e79428655d1d
ASAGNORI <angelo.sagnori@gmail.com>
2025-03-05 15:55:17 -0300

    New code committed

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,36 @@
-print("Hello World Real")
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+st.set_page_config(layout="wide")
+
+var_str = ""
+var_us = "USA"
+var_it = "Italy"
+var_br = "Brazil"
+var_hello = "Hello World Real:"
+ 
+list_1 = [var_hello, var_us, var_it, var_br]
+
+
+for x in list_1 :
+    var_str = var_str + x + " "
+    
+st.write(var_str)
+
+st.divider()
+
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_top_100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+v_price_max = df_top_100_books["book price"].max()
+v_price_min = df_top_100_books["book price"].min()
+v_max_price = st.sidebar.slider("Price Range", v_price_min, v_price_max, v_price_max, )
+
+df_books = df_top_100_books[df_top_100_books["book price"] <= v_max_price]
+st.write(df_books)
+
+v_graph1 = px.bar(df_books["year of publication"].value_counts())
+v_graph2 = px.bar(df_books["book price"])
+
+col1, col2 = st.columns(2)
+col1.plotly_chart(v_graph1)
+col2.plotly_chart(v_graph2)
---
+++ pages/book_reviews.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_top_100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+v_books = df_top_100_books["book title"].unique()
+v_book_sel = st.sidebar.selectbox("Books: ", v_books)
+
+df_books = df_top_100_books[df_top_100_books["book title"] == v_book_sel]
+df_reviews_f = df_reviews[df_reviews["book name"] == v_book_sel]
+
+v_book_title  = df_books["book title"].iloc[0]
+v_book_genre  = df_books["genre"].iloc[0]
+v_book_price  = f"${df_books['book price'].iloc[0]}"
+v_book_rating = df_books["rating"].iloc[0]
+v_book_year   = df_books["year of publication"].iloc[0]
+
+st.title(v_book_title)
+st.subheader(v_book_genre)
+col1,col2,col3 = st.columns(3)
+col1.metric("Price", v_book_price)
+col2.metric("Rating", v_book_rating)
+col3.metric("Year", v_book_year)
+
+st.divider()
+st.subheader("Reviews: ")
+
+for rows in df_reviews_f.values : 
+    v_message = st.chat_message(f"{rows[4]}")
+    v_message.write(f"**{rows[2]}**")
+    v_message.write(rows[5])
